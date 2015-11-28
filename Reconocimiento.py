3d7bd68199ca06e59dc81344c20a4d462308549c
Fermín García <nimref777@gmail.com>
2015-11-28 23:59:38 +0100

    facial + arduino
    all faces

--- Reconocimiento.py
+++ Reconocimiento.py
@@ -1,35 +1,41 @@
+import serial
 import numpy as np
 import cv2
- 
-#cargamos la plantilla e inicializamos la webcam:
+import threading
+#charge template and init webcam
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
-encontrado = False
- 
-while(not encontrado):
-    #leemos un frame y lo guardamos
+findFace=False
+
+def lecturaSerial():
+    ser = serial.Serial('/dev/ttyUSB0', 9600) #We initialize the serial port 9600 baud
+    cod ="04 26 56 AA 10 2C 80" #target code
+    #variable to store the message
+    #we assign a value entered by the user
+    frase=""
+    while not(cod in frase): 
+        frase = ser.readline()
+    return frase
+
+t = threading.Thread(target=lecturaSerial)
+t.start()
+
+while (t.isAlive() or not findFace):
+    #read frame and save
     ret, img = cap.read()
- 
-    #convertimos la imagen a blanco y negro
+    #convert img to white and black
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
- 
-    #buscamos las coordenadas de los rostros (si los hay) y
-    #guardamos su posicion
+    #find faces and save position
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
- 
-    #Dibujamos un rectangulo en las coordenadas de cada rostro
+    #Print rectangle in faces
+    
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(125,255,0),2)
- 
-    #Mostramos la imagen
+    #Show img
     cv2.imshow('img',img)
     
     if len(faces)!= 0:
-    encontrado = True
-     
-    #con la tecla 'q' salimos del programa
+        findFace=True
+
     cv2.waitKey(1)
-
-
 cap.release()
-cv2-destroyAllWindows()
