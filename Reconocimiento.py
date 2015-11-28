9b24ccd19c43b49c66dbf5b427064b607bbb4698
Fermín García <nimref777@gmail.com>
2015-11-28 07:01:30 +0100

    Cuando reconoce cara de apaga

--- Reconocimiento.py
+++ Reconocimiento.py
@@ -4,8 +4,9 @@ import cv2
 #cargamos la plantilla e inicializamos la webcam:
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
+encontrado = False
  
-while(True):
+while(not encontrado):
     #leemos un frame y lo guardamos
     ret, img = cap.read()
  
@@ -19,17 +20,16 @@ while(True):
     #Dibujamos un rectangulo en las coordenadas de cada rostro
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(125,255,0),2)
-
-    if not faces.notEmpty()
-        break
  
     #Mostramos la imagen
     cv2.imshow('img',img)
+    
+    if len(faces)!= 0:
+    encontrado = True
      
     #con la tecla 'q' salimos del programa
     cv2.waitKey(1)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+
 
 cap.release()
 cv2-destroyAllWindows()
