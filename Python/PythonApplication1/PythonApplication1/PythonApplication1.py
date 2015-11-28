73c8a14238ed4554f0bb0681c258427665776d76
MikeSnow <mkgarcis@gmail.com>
2015-11-28 14:43:41 +0100

    Subida de metodo para leer de rfid por python

---
+++ Python/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,12 @@
+﻿import serial #cargamos la libreria serial
+
+def lecturaSerial():
+    ser = serial.Serial('COM4', 9600) #inicializamos el puerto de serie a 9600 baud
+    #variable para almacenar el mensaje
+    #le asignamos un valor introducido por el usuari
+    frase=""
+    frase = ser.readline()
+    print (frase)
+       #ser.write(entrada) #envia la entrada por serial
+    return frase
+  
