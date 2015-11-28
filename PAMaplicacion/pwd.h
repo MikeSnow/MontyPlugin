f1336be9d69c64dff9f149e0cd7d7349a77602a7
jmartinman <j.martinmansilla@gmail.com>
2015-11-28 10:38:13 +0100

    Subido Código aplicación PAM login
    Se sube el código  de PAM para el login

---
+++ PAMaplicacion/pwd.h
@@ -0,0 +1,9 @@
+struct passwd{
+	char *pw_name;  //user name
+	char *pw_passwd;//user password
+	uid_t pw_uid;//user id
+	git_t pw_gid;//group id
+	char *pw_gecos;//real name
+	char *pw_dir;//home directory
+	char *pw_shell;//shell program
+};
